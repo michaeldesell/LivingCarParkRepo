0772f34dba3473f8a77cb4139169e11a5b952fb3
Michael Desell <michael.desell@claremont.se>
2019-01-16 15:24:50 +0100

    Added entities and started on dbcontext

---
+++ LivingCarPark/Data/LivingCarParkContext.cs
@@ -0,0 +1,16 @@
+﻿using LivingCarPark.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LivingCarPark.Data
+{
+    public class LivingCarParkContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Carpark> Carparks { get; set; }
+
+    }
+}
--- LivingCarPark/Startup.cs
+++ LivingCarPark/Startup.cs
@@ -2,19 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LivingCarPark.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LivingCarPark
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+
+            _config = config;
+
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<LivingCarParkContext>();
             services.AddMvc();
         }
 
